a2570f3b806470c30ab6ea9736352f54daee6456
Agustin <pereyra.agus@outlook.com.ar>
2023-02-02 01:43:55 -0300

    se crearon archivos js para los eventos

---
+++ Ejercicios PDF/pdf1.js
@@ -0,0 +1,203 @@
+/* ---- INTRO A JAVASCRIPT ---- */
+
+/* Ejercicio 1 */
+// let miNombre = "Agustin"
+
+/*Ejercicio 2 */
+// let miApellido = "Pereyra"
+
+/* Ejercicio 3 */
+// let miEdad = 27
+
+/*Ejercicio 4 */
+// let miMascota = "Bafi"
+
+/* Ejercicio 5 */
+// let edadMascota = 10
+
+/* Ejercicio 6 */
+// console.log(miNombre)
+// console.log(miApellido)
+// console.log(miEdad)
+// console.log(miMascota)
+// console.log(edadMascota)
+
+/* Ejercicio 7 */
+// let nombreCompleto = (miNombre + " " + miApellido)
+
+/* Ejercicio 8 */
+// let textoPresentacion = "Mi nombre es " + [[miNombre] + " " + [miApellido]] + " " + "tengo" + " " + [miEdad] + " " + "años" + " " + "mi mascota se llama" + " " + [miMascota] + " " + "y tiene" + " " + [edadMascota] + " " + "años"
+
+// console.log(textoPresentacion)
+
+/* Ejercicio 9 */
+// let sumaEdades = ((miEdad) + (edadMascota))
+
+// console.log(sumaEdades)
+
+// let restaEdades = ((miEdad) - (edadMascota))
+
+// console.log(restaEdades)
+
+// let productoEdades = ((miEdad) / (edadMascota))
+
+// console.log(productoEdades)
+
+// let divisionEdad = ((miEdad) % (edadMascota))
+
+// console.log(divisionEdad)
+
+/* Ejercicio 10 */
+// let nombre = prompt("Ingrese su nombre")
+
+// let apellido = prompt("Ingrese su apellido")
+
+// let edad = prompt("Ingrese su edad")
+
+// let mascota = prompt("Ingrese el nombre de su mascota")
+
+// let mascotaEdad = prompt("Ingrese la edad de su mascota")
+
+// console.log((nombre) + " " + (apellido) + " " + (edad) + " " + (mascota) + " " + (mascotaEdad))
+
+/* Ejercicio 11 */
+// const alumno = {
+//     nombre : "Agustin",
+//     apellido : "Pereyra",
+//     edad : 27,
+//     curso : "JavaScript",
+//     pais : "Argentina"
+// }
+
+// console.table(alumno)
+
+// console.log(alumno.nombre)
+// console.log(alumno.apellido)
+// console.log(alumno.edad)
+// console.log(alumno.curso)
+// console.log(alumno.pais)
+
+/* Ejercicio 12 */
+// const mascota = {
+//     nombre : "Bafi",
+//     tipo : "Gato",
+//     edad : 10,
+//     hobby : "Tomar sol",
+//     nohobby : "Que le toquen la pancita"
+// }
+
+// console.table(mascota)
+
+// console.log(mascota.nombre)
+// console.log(mascota.tipo)
+// console.log(mascota.edad)
+// console.log(mascota.hobby)
+// console.log(mascota.nohobby)
+
+/* Ejercicio 13 */
+// const frutas = ["banana", "manzana", "pera", "naranja", "frutilla"]
+
+// console.log(frutas)
+
+// console.log(frutas[0])
+// console.log(frutas[1])
+// console.log(frutas[2])
+// console.log(frutas[3])
+// console.log(frutas[4])
+
+/* Ejercicio 14 */
+// const numeros = [1,2,3,4,5,]
+
+// console.log(numeros)
+
+// console.log(numeros[0])
+// console.log(numeros[1])
+// console.log(numeros[2])
+// console.log(numeros[3])
+// console.log(numeros[4])
+
+/* Ejercicio 15 */
+// const familia = ["Olma", "Cecilia", "Sebastian", "Nadia", "Agustin"]
+
+// console.log(familia)
+
+// console.log(familia[0])
+// console.log(familia[1])
+// console.log(familia[2])
+// console.log(familia[3])
+// console.log(familia[4])
+
+/* Ejercicio 16 */
+// let textoAleatorio = ((frutas[1]) + " " + (numeros[3]) + " " + (familia[4]))
+
+// console.log(textoAleatorio)
+
+/* Ejercicio 17 */
+// let miedad = prompt("Ingresa tu edad")
+
+// let suedad = prompt("Ingresa la edad de tu compañero")
+
+// if (miedad > suedad) {
+//     console.log("Soy mayor.")
+// } else if (miedad < suedad) {
+//     console.log("Soy menor.")
+// }
+// else {
+//     (miedad === suedad)
+//     console.log("Edades iguales")
+// }
+
+// let edadesIguales = miedad === suedad
+
+// let soyMayor = miedad > suedad
+
+// let soyMenor = miedad < suedad
+
+// console.log(edadesIguales)
+// console.log(soyMayor)
+// console.log(soyMenor)
+
+/* Ejercicio 18 */
+// let myAge = prompt("Ingresa tu edad")
+
+// let soyMayorDeEdad = myAge
+
+// if (myAge >= 18) {
+//     console.log(`Soy mayor de edad, mi edad es ${soyMayorDeEdad}`)
+// } else {
+//     console.log("Soy menor de edad")
+// }
+
+/* Ejercicio 19 */
+// let edad = prompt("Ingrese su edad")
+// let altura = prompt("Ingrese su altura en cms")
+
+// let suedad = edad
+// let sualtura = altura
+
+// let puedeSubir = (edad > 6) && (altura >=120)
+// console.log(`Puede subir a la atraccion, su edad es ${edad} años, y su altura es ${altura} cms`)
+
+/* Ejercicio 20 */
+// let pase = prompt("Ingrese el nombre de su pase")
+// let saldo = prompt("Ingrese el saldo que dispone")
+
+// let supase = pase
+// let susaldo = saldo
+
+// if((pase === "vip") || (saldo > 1000)){
+//     console.log(`La persona puede pasar, su pase es ${pase}, y su saldo es ${saldo}`)
+//     alert(`La persona puede pasar, su pase es ${pase}, y su saldo es ${saldo}`)
+// } else if ((pase !== "vip") || (saldo < 1000)){
+//     console.log(`La persona no puede pasar, su pase es ${pase}, y su saldo es ${saldo}`)
+//     alert(`La persona no puede pasar, su pase es ${pase}, y su saldo es ${saldo}`)
+// } else{
+//     console.log("Ingrese valores")
+//     alert("Ingrese valores")
+// }
+
+// let puedePasar =`Su pase es ${supase} y su saldo es ${susaldo}`
+// console.log(puedePasar)
+// alert(puedePasar)
+
+/* ------------------- */
---
+++ scripts/upComing.js
@@ -0,0 +1,22 @@
+const cards = document.getElementById("section-cards")
+let info = data.events
+// let card = " "
+
+for(let infoEvent of info){
+    if(infoEvent.date > data.currentDate){
+        cards.innerHTML += `<div class="card m-2" style="width: 16rem; height: 24rem;">
+        <img src=${infoEvent.image} class="card-img-top mt-2" alt="food">
+        <div class="card-body d-flex flex-column justify-content-between">
+          <div>
+            <h5 class="card-title">${infoEvent.name}</h5>
+            <p class="card-text">${infoEvent.description}</p>
+          </div>
+          <div class="d-flex justify-content-between align-items-center">
+            <span>Price: $${infoEvent.price}</span>
+            <a href="./pages/details.html" class="btn btn-outline-secondary">Go details</a>
+          </div>
+        </div>
+      </div>`
+    }
+}
+// cards.innerHTML = card
